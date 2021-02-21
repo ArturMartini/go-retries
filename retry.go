65b4cc9a8e32a95a7f76cb56518c3f1f1bc238ff
Artur Martini <artur.martini@bexsbanco.com.br>
2021-02-20 21:04:48 -0300

    Adding first logic to retries

--- retry.go
+++ retry.go
@@ -1,1 +1,65 @@
 package go_retries
+
+import (
+	"errors"
+	"time"
+)
+
+const (
+	defaultMaxRetries int = 3
+	defaultDelaySec   int = 3
+
+	ConfigMaxRetries Config = "max.retries"
+	ConfigDelaySec   Config = "delay.sec"
+)
+
+var (
+	listUnrecoverableErrors []error
+
+	configs map[Config]int = map[Config]int{
+		ConfigMaxRetries: defaultMaxRetries,
+		ConfigDelaySec: defaultDelaySec,
+	}
+
+	ErrorMaxRetriesReached = errors.New("max retries reached")
+	ErrorUnrecoverable     = errors.New("unrecoverable error")
+)
+
+type Config string
+
+func Setting(configurations map[Config]int, unrecoverableErrors []error) {
+	configs = configurations
+	listUnrecoverableErrors = unrecoverableErrors
+}
+
+func Do(f func() error) error {
+	var retry = 0
+
+	for {
+		if retry > configs[ConfigMaxRetries] {
+			return ErrorMaxRetriesReached
+		}
+
+		err := f()
+		if err != nil {
+			if isUnrecoverableErrors(err) {
+				return ErrorUnrecoverable
+			} else {
+				<-time.After(time.Second * time.Duration(configs[ConfigDelaySec]))
+				retry++
+			}
+		} else {
+			return nil
+		}
+	}
+}
+
+func isUnrecoverableErrors(err error) bool {
+	var isUnrecoverable = false
+	for _, recErr := range listUnrecoverableErrors {
+		if errors.Is(err, recErr) {
+			isUnrecoverable = true
+		}
+	}
+	return isUnrecoverable
+}
--- retry_test.go
+++ retry_test.go
@@ -1,1 +1,40 @@
 package go_retries
+
+import (
+	"errors"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultRetry(t *testing.T) {
+
+	err := Do(func() error {
+		return nil
+	})
+
+	assert.Nil(t, err)
+
+
+	var retry = 0
+
+	err = Do(func() error {
+		if retry < 2 {
+			retry++
+			return errors.New("recoverable")
+		} else {
+			return nil
+		}
+	})
+
+	assert.Equal(t, 2, retry)
+	assert.Nil(t, err)
+
+
+	unrecoverableError := errors.New("unrecoverable")
+	Setting(nil, []error{unrecoverableError})
+	err = Do(func() error {
+		return unrecoverableError
+	})
+
+	assert.Error(t, ErrorUnrecoverable, err)
+}
